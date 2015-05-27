489b812feb71679de4d143851a0dc2b05351b2ee
Sean P. Kane <skane@newrelic.com>
2015-05-27 22:24:04 +0000

    Add apache2 cookbook

--- chef-repo/cookbooks/app/recipes/default.rb
+++ chef-repo/cookbooks/app/recipes/default.rb
@@ -4,10 +4,12 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
+include_recipe 'apache2
+include_recipe 'app::mysql_service'
+
 package 'httpd'
 
 service 'httpd' do
   action [ :enable, :start ]
 end
 
-include_recipe 'app::mysql_service'
